5212aa4d11afee9260c902e53fbc2160102d541b
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2023-11-21 22:17:14 +0000

    create navbar

--- web/src/App.js
+++ web/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home.jsx';
 import Projects from './Components/Projects.jsx';
 import Experience from './Components/Experience.jsx';
+import Navbar from './Components/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route patch="/" element={<Home />} />
           <Route patch="/projects" element={<Projects />} />
