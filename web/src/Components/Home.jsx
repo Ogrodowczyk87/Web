88667e486a2bd65b7a4db3c3210e53bd715f1561
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2023-12-19 22:10:26 +0000

    add temporary layot to home page

--- web/src/Components/Home.jsx
+++ web/src/Components/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import portret from '../Pictures/portret.jpg'
+import portret from "../Pictures/portret.jpg"
 
 function Home() {
-    return <div>
+    return <div className="home_wrapper">
         <div className="home">
-            <div>
+            <div className="home_description">
                 <h1 className="home_title">Hello, I'm Rafal
-                    I'm a <span>Junior Front End developer</span>
+                    I'm a <span className="home_span">Junior Front End developer</span>
                 </h1>
                 <p className="home_subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                     Tenetur culpa dolorem velit, a iusto aspernatur.
@@ -15,7 +15,7 @@ function Home() {
             </div>
         </div>
         <div>
-            <img src={portret} />
+            <img className="picture" src={portret} alt="portret" />
         </div>
     </div>;
 }
