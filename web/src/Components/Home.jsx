cc7145f8db03f9dce3b09209edd41331b193a3ea
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2023-12-19 21:47:02 +0000

    small update

--- web/src/Components/Home.jsx
+++ web/src/Components/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import portret from '../Pictures/portret.jpg'
 
 function Home() {
-    return <div className="home">Home!</div>;
+    return <div>
+        <div className="home">
+            <div>
+                <h1 className="home_title">Hello, I'm Rafal
+                    I'm a <span>Junior Front End developer</span>
+                </h1>
+                <p className="home_subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Tenetur culpa dolorem velit, a iusto aspernatur.
+                </p>
+                <div>project bottom or animation</div>
+            </div>
+        </div>
+        <div>
+            <img src={portret} />
+        </div>
+    </div>;
 }
 
 export default Home;
