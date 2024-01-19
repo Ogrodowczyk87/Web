8fdc16cce5b58ea4303aaad889070e497a25e1ea
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2024-01-19 16:14:19 +0000

    scss

--- web/src/Components/Projects.jsx
+++ web/src/Components/Projects.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Card from "./sub/Card"
 
 function Projects() {
-    return <div className="projects">Projects</div>;
+
+    return <div className="projects">
+        <div className="project-title">
+            <h3>My work</h3>
+        </div>
+        <div className="card-wrapper">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    </div>;
 }
 
 export default Projects;
