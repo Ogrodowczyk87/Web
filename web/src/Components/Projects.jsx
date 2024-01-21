01099b143318493d1889d0a0b12c3c4ef36e29a2
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2024-01-21 18:26:27 +0000

    cards example

--- web/src/Components/Projects.jsx
+++ web/src/Components/Projects.jsx
@@ -1,19 +1,41 @@
 import React from "react";
-import Card from "./sub/Card"
+import  portret  from "../Assets/Images/portret.jpg"
 
-function Projects() {
+function Card() {
 
+    const data = [
+        {
+            name: 'Ice Scream Shop',
+            img:  portret ,
+            description: 'lorem10'
+        },
+        {
+            name: 'Software house',
+            img:  portret ,
+            description: 'lorem10'
+        },
+        {
+            name: 'Film Service',
+            img:  portret ,
+            description: 'lorem10'
+        }
+    ]
     return <div className="projects">
-        <div className="project-title">
-            <h3>My work</h3>
-        </div>
-        <div className="card-wrapper">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+        <div>
+            {data.map((data) => (
+                <div className="card-container">
+                    <div className="image-container">
+                        <img className="projects-image" src={data.img} alt="" />
+                    </div>
+                    <div className="card-description">
+                        <p>{data.name}</p>
+                        <p>{data.description}</p>
+                        <button>Git hub</button>
+                    </div>
+                </div>
+            ))}
         </div>
     </div>;
 }
 
-export default Projects;
+export default Card;
