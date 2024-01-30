8e37a230b67b554890d660deb2e0f09f16f2c09c
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2024-01-30 19:27:38 +0000

    layout update

--- web/src/Components/AboutMe.jsx
+++ web/src/Components/AboutMe.jsx
@@ -3,7 +3,8 @@ import avatar from "../Assets/Images/avatar.jpg"
 
 function AboutMe() {
     return (
-        <div className="AboutMe_container">AboutMe
+        <div className="aboutMe_container">
+            <h2 className="aboutMe_title">AboutMe</h2>
             <div className="home_picture">
                 <img className="picture" src={avatar} alt="portret" />
             </div>
--- web/src/Components/Home.jsx
+++ web/src/Components/Home.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import github from '../Assets/Images/github.svg'
+import { motion, useTime, useTransform } from "framer-motion"
 
 function Home() {
+
+    const time = useTime()
+    const rotate = useTransform(
+        time,
+        [0, 4000], // For every 4 seconds...
+        [0, 360], // ...rotate 360deg
+        { clamp: false }
+    )
+
     return <div className="home_wrapper">
         <div className="home">
-            <div className="home_svg">
+            <motion.div className="home_svg"
+                style={{ rotate }}
+            >
                 <a href="https://github.com/Ogrodowczyk87" target="_blank" >
                     <img src={github} alt="Coding Beauty logo"></img>
                 </a>
-            </div>
+            </motion.div>
             <div className="home_description">
                 <h1 className="home_title">Hello </h1>
                 <p className="home_myName">  I'm Rafal Ogrodowczyk</p>
@@ -16,7 +28,7 @@ function Home() {
             </div>
         </div>
 
-    </div>;
+    </div >;
 }
 
 export default Home;
