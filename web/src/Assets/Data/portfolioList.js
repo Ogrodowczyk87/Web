6505818c2dc1a8417295c483255971d58f1a251f
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2024-02-12 16:06:53 +0000

    portfolioList

---
+++ web/src/Assets/Data/portfolioList.js
@@ -0,0 +1,24 @@
+
+const portfolioList = [
+    {
+        id: 1,
+        name: "SoftwareHouse",
+        src: "../Images/portret.jpg",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
+    },
+    {
+        id: 2,
+        name: "IceScream Shop",
+        src: "../Images/portret.jpg",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
+    },
+    {
+        id: 3,
+        name: "FilmWeb Services",
+        src: "../Images/portret.jpg",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
+    },
+
+]
+
+export default portfolioList
